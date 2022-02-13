0cf0171cc461cdc119bb14401930441062e93d7d
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-02-13 11:34:41 -0300

    Finalização capitulo 1

--- main.go
+++ main.go
@@ -3,15 +3,38 @@ package main
 import "fmt"
 
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
+const prefixoOlaIngles = "Hello, "
+const espanhol = "espanhol"
+const frances = "frances"
+const ingles = "ingles"
 
-func Ola(nome string) string {
+func prefixoSaudacao(idioma string) (prefixo string) {
+
+	switch idioma {
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+	case frances:
+		prefixo = prefixoOlaFrances
+	case ingles:
+		prefixo = prefixoOlaIngles
+	default:
+		prefixo = prefixoOlaPortugues
+	}
+	return
+
+}
+
+func Ola(nome string, idioma string) string {
 
 	if nome == "" {
 		nome = "mundo"
 	}
-	return prefixoOlaPortugues + nome
+
+	return prefixoSaudacao(idioma) + nome
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("mundo", ""))
 }
