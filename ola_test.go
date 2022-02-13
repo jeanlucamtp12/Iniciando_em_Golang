e3136e0ab7064fb7f98d4941b684ff4b634b04bc
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-02-13 11:35:57 -0300

    Finalização capitulo 1

--- ola_test.go
+++ ola_test.go
@@ -4,27 +4,52 @@ import "testing"
 
 func TestOla(t *testing.T) {
 
-	verifcaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
 		t.Helper()
 		if resultado != esperado {
 			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 		}
 
 	}
+
+	t.Run("em ingles", func(t *testing.T) {
+		resultado := Ola("Matt", "ingles")
+		esperado := "Hello, Matt"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+
+	})
+
+	t.Run("em frances", func(t *testing.T) {
+		resultado := Ola("Jeanluca", "frances")
+		esperado := "Bonjour, Jeanluca"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+
+		resultado := Ola("Elodie", "espanhol")
+		esperado := "Hola, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+
+	})
+
 	t.Run("diga olá para as pessoas", func(t *testing.T) {
 
-		resultado := Ola("Chris")
+		resultado := Ola("Chris", "")
 		esperado := "Olá, Chris"
 
-		verifcaMensagemCorreta(t, resultado, esperado)
+		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("diga 'Olá, mundo' caso a string passada seja vazia", func(t *testing.T) {
 
-		resultado := Ola("")
+		resultado := Ola("", "")
 		esperado := "Olá, mundo"
 
-		verifcaMensagemCorreta(t, resultado, esperado)
+		verificaMensagemCorreta(t, resultado, esperado)
 
 	})
 
