a7fef9796becb93f2dffb36cf42153e27be56daf
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2022-02-12 21:28:36 -0300

    alteração no arquivo de teste

--- ola_test.go
+++ ola_test.go
@@ -3,11 +3,29 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Chris")
-	esperado := "Olá, Chris"
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verifcaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+
 	}
+	t.Run("diga olá para as pessoas", func(t *testing.T) {
+
+		resultado := Ola("Chris")
+		esperado := "Olá, Chris"
+
+		verifcaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diga 'Olá, mundo' caso a string passada seja vazia", func(t *testing.T) {
+
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		verifcaMensagemCorreta(t, resultado, esperado)
+
+	})
 
 }
